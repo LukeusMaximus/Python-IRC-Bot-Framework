6324a58e6d5e9a09d92fe6cc3e76919a8b7b8722
Lukeus_Maximus <github@lukemurray.co.uk>
2011-09-15 12:22:52 +0100

    modified example to get around synirc "PING whilst connecting" problem

--- examplebot.py
+++ examplebot.py
@@ -19,6 +19,12 @@ def saySuccess(bot, channel, message):
 def kickSuccess(bot, nick, channel, reason):
     bot.kick(nick, channel, reason)
 
+def identPass(bot):
+    pass
+    
+def identFail(bot):
+    pass
+
 def privmsg(bot, sender, headers, message):
     if message.startswith("!say "):
         firstSpace = message[5:].find(" ") + 5
@@ -38,9 +44,20 @@ def privmsg(bot, sender, headers, message):
         secondSpace = message[firstSpace+1:].find(" ") + (firstSpace + 1)
         if sender == owner:
             bot.identify(sender, kickSuccess, (message[6:firstSpace], message[firstSpace+1:secondSpace], message[secondSpace+1:]), authFailure, (headers[0], sender))
-
+            
 def actionmsg(bot, sender, headers, message):
     print "An ACTION message was sent by " + sender + " with the headers " + headers + ". It says: \"" + sender + " " + message
+
+def endMOTD(bot, sender, headers, message):
+    bot.join(chanName)
+    bot.say(chanName, "I am an example bot.")
+    bot.say(chanName, "I have 4 functions, they are Join, Kick, Quit and Say.")
+    bot.say(chanName, "Join (joins a channel); Usage: \"!join #<channel>\"")
+    bot.say(chanName, "Kick (kicks a user); Usage: \"!kick <nick> #<channel> <reason>\"")
+    bot.say(chanName, "Quit (disconnects from the IRC server); Usage: \"!quit [<quit message>]\"")
+    bot.say(chanName, "Say (makes the bot say something); Usage: \"!say <channel/user> <message>\"")
+    bot.say(chanName, "The underlying framework is in no way limited to the above functions.")
+    bot.say(chanName, "This is merely an example of the framework's usage")
 
 # Main program begins here
 if __name__ == "__main__":
@@ -50,16 +67,8 @@ if __name__ == "__main__":
         bot = ircBot("irc.synirc.net", 6667, "ExampleBot", "An example bot written with the new IRC bot framework")
         bot.bind("PRIVMSG", privmsg)
         bot.bind("ACTION", actionmsg)
+        bot.bind("376", endMOTD)
         bot.connect()
-        bot.join(chanName)
-        bot.say(chanName, "I am an example bot.")
-        bot.say(chanName, "I have 4 functions, they are Join, Kick, Quit and Say.")
-        bot.say(chanName, "Join (joins a channel); Usage: \"!join #<channel>\"")
-        bot.say(chanName, "Kick (kicks a user); Usage: \"!kick <nick> #<channel> <reason>\"")
-        bot.say(chanName, "Quit (disconnects from the IRC server); Usage: \"!quit [<quit message>]\"")
-        bot.say(chanName, "Say (makes the bot say something); Usage: \"!say <channel/user> <message>\"")
-        bot.say(chanName, "The underlying framework is in no way limited to the above functions.")
-        bot.say(chanName, "This is merely an example of the framework's usage")
         bot.run()
     else:
         print "Usage: python examplebot.py <your IRC nick> <irc channel (no '#' character please)>"
